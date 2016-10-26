fb242685bb494658bff3632b6fe5a5651e2f7fd4
Kelsey Hammer <khammer19@gmail.com>
2016-10-26 18:12:12 +0000

    dear hauser you're going to look at this committ and say, why did you not commit all along? and you are right sir! I'm so excited thought because I was very confused before and suddenly things are clicking. That being said: wrote script, updated script, added image, added personal file

---
+++ kelhammer-convert-docs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Declare input argument as a variable
+  echo "What file type would you like?"
+ 
+ # read in the name 
+  read FILE
+  
+ #say file name
+   echo "You would like a $FILE! This will take a little work. Type any command to begin conversion"
+   
+ # read in the name 
+  read FILE
+  
+# Convert markdown to HTML
+pandoc -S -o kelhammer.html kelhammer.md 
+
+# Declare input argument as a variable
+  echo "The document has been converted from md. to html. to continue type any command"
+ 
+ # read in the name 
+  read FILE
+  
+ #say file name
+   echo "We will now convert"
+
+# Convert markdown to DOCX
+
+pandoc -S -o kelhammer.docx kelhammer.md
+
+# Declare input argument as a variable
+  echo "The document has been converted from md. to .docx to continue type any command"
+ 
+ # read in the name 
+  read FILE
+  
+ #say file name
+   echo "We will now convert"
+
+# Convert markdown to ODT
+pandoc -S -o kelhammer.odt kelhammer.md
+
+# Declare input argument as a variable
+  echo "The document has been converted from .md to .odt to continue type any command"
+ 
+ # read in the name 
+  read FILE
+  
+ #say file name
+   echo "We will now convert"
+
+# Convert markdown to PDF
+pandoc -S -o kelhammer.pdf kelhammer.md
+
+# Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF"
